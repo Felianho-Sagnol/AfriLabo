8e9ccd0e6656323c401297837510306a17b8b5b3
Mamadou Cissé <mamadouibncisse@gmail.com>
2021-09-21 02:02:33 +0200

    changement du nom de la route de reception et son nommage

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('Accueil');
 });
-<<<<<<< HEAD
-
-Route::get('/Reception',function(){ return view('Reception/Reception'); });
-=======
->>>>>>> 8667687600988d25c2c68b70b3ac6378b7b44d6c
+Route::get('/Reception',function(){ return view('Reception/Reception'); })->name('reception');
