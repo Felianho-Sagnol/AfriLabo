c59377052d0848b11c2b1a9aefd0197fc1470a24
Félix SAGNOL <gobou0303@gmail.com>
2021-10-03 18:41:49 +0200

    Traitement du popup

--- public/Js/layoutJs/popupController.js
+++ public/Js/layoutJs/popupController.js
@@ -1,20 +1,29 @@
-    $(function(){
-        $("#apercu").click(function(){
-            if($( "i" ).hasClass( "bi-eye-fill" )){
+    $(function() {
+        $("#apercu").on("click", function() {
+            if ($("i").hasClass("bi-eye-fill")) {
                 console.log("oiel active")
-                $( " i" ).removeClass( "bi-eye-fill" ).delay( 300 ).addClass( "bi-eye-slash" );
+                $(" i").removeClass("bi-eye-fill").delay(300).addClass("bi-eye-slash");
                 $("#layoutAppercue").show(200).fadeIn(200)
                 $('.headBar').css({
-                    "filter":"blur(10px)"
+                    "filter": "blur(10px)"
                 })
-            }
-            else{
-                $( " i" ).removeClass( "bi-eye-slash" ).delay( 300 ).addClass( " bi-eye-fill");
+            } else {
+                $(" i").removeClass("bi-eye-slash").delay(300).addClass(" bi-eye-fill");
                 $("#layoutAppercue").hide(200).fadeOut(200)
-
             }
         })
     })
-    $(".bi-eye-slash").click(function(){
-        console.log("bien fermer")
-    })
+
+    export function getDemandeEchantillonsInfos(demandeId) {
+        let modificationUrl = "http://127.0.0.1:8000/getDemande"
+        $.get(modificationUrl, { demandeId: demandeId }, (data) => {
+            localData = data
+        }).then(() => {
+            if (localData.demandeExist) {
+                let echantillons = localData.echantillons
+                console.log(echantillons)
+
+                //remplir le popup ici avec echantillons
+            }
+        })
+    }
